c167fa0b4ea43236354ad706d3da3102b13dc858
Rafael Ribas <133046889+profRafaelRibas@users.noreply.github.com>
2024-05-24 15:54:18 -0300

    24052024

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,34 @@
 import Link from 'next/link'
+import styles from '../styles/Header.module.css'
 
-export default function Header() {
+export default function Header(props) {
+    let tamanho
+    if(props.size=="big"){
+        tamanho = "22pt"
+    }else{
+        tamanho = "16pt"
+    }
     return (
-        <header>
-            <nav>
-                <Link href="/">
-                    Home
-                </Link>
-                <Link href="/about">
-                    Sobre
-                </Link>
-            </nav>
+        <header className={styles.header} style={{
+            backgroundColor:props.bgcolor
+        }} >
+            <aside className={styles.aside}>
+                <img src="logo/logo2.png" className={styles.img}/>
+                <nav className={styles.nav}>
+                    <Link href="/" className={styles.a}>
+                        Home
+                    </Link>
+                    <Link href="/produtos" className={styles.a}>
+                        Produtos
+                    </Link>
+                    <Link href="/contato" className={styles.a}>
+                        Contato
+                    </Link>
+                </nav>
+            </aside>
+            <h1 style={{
+                fontSize:tamanho
+            }}>{props.title}</h1>
         </header>
     )
 }
--- pages/primeira.js
+++
@@ -1,6 +0,0 @@
-export default function primeira() {
-    const nome = "Rafael"
-    const menssagem = "Bom dia "
-    return <h1>{menssagem}{nome}. Bem vindo ao curso de React</h1>
-}
-
